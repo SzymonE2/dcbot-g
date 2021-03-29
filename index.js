2ccc62718d96a0e813c1db96dfd605faa7734c2c
UKJ1 <81575943+UKJ1@users.noreply.github.com>
2021-03-29 16:54:49 +0200

    program

---
+++ index.js
@@ -0,0 +1,61 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === '/afk') {
+
+  }
+});
+
+/*
+client.on('voiceStateUpdate', (oldState, newState) => {
+    // check for bot
+    //if (oldState.member.user.bot) return;
+
+    const channel = client.channels.cache.find(channel => channel.name === 'ogolne');
+    channel.send('@everyone aaaa');
+})
+*/
+
+
+
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+    let newUserChannel = newMember.channelID;
+    let oldUserChannel = oldMember.channelID;
+
+    const channel2 = client.channels.cache.get('776728263334035476');
+ 
+    if(newUserChannel === "824713613972733994")
+    { 
+        // User Joins a voice channel
+        channel2.send("Hej @everyone! Ktoś czeka właśnie na rozmowę. Dołączcie!");
+    }
+    else{
+        // User leaves a voice channel
+        //channel2.send("Nikt już nie czeka na rozmowę :(");
+    }
+ });
+
+ /*
+
+ client.on('voiceStateUpdate', (oldMember, newMember) => {
+    let newUserChannel = newMember.channelID;
+    let oldUserChannel = oldMember.channelID;
+
+    const channel2 = client.channels.cache.get('821364012033376289');
+ 
+    if(newUserChannel === "821364012033376289")
+    { 
+        if(voiceChannel.members.size >= 2){
+            client.move_member(message.author, channel)
+        }
+    }
+ });
+*/
+
+
+client.login('ODI1NDQ4ODQ4MjMwNzExMzY2.YF-FJQ.zMzKD_WQyyDbZUtR9Y2fBiCEABI');
